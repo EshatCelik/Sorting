aa52fc7025ebe36105843a31d77f91aab8713df6
Eshat ÇELİK <eshat.celik@procenne.com>
2023-07-20 13:20:47 +0300

    Add project files.

---
+++ Sorting.BubleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sorting.BubleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 5, 2, 11, 6, 7, 4, 8, 33,1 };
+           //               { 2, 5, 3, 11, 6, 7, 4, 8, 33 };
+           //               { 2, 3, 5, 11, 6, 7, 4, 8, 33 };
+           //               { 2, 3, 4, 11, 6, 7, 5, 8, 33 };
+
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+
+                    }
+                }
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] +" -");
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
